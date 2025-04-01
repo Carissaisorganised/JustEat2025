a318c911f1f7e97278d785115909f5342bce2bc9
Carissa Nicholls <92162465+Carissaisorganised@users.noreply.github.com>
2025-04-01 12:50:37 +0100

    Initial Commit

---
+++ JustEat2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JustEat2025
+//
+//  Created by Carissa Nicholls on 01/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JustEat2025/JustEat2025App.swift
@@ -0,0 +1,17 @@
+//
+//  JustEat2025App.swift
+//  JustEat2025
+//
+//  Created by Carissa Nicholls on 01/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct JustEat2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
