a6141d7cd36c4db99fde3be02f12179b82875672
Carissa Nicholls <92162465+Carissaisorganised@users.noreply.github.com>
2025-04-01 13:19:15 +0100

    adding search bar to main page

--- JustEat2025/ContentView.swift
+++ JustEat2025/ContentView.swift
@@ -8,17 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Image(systemName: "justeat")//put just eat logo
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height:100)
+                .padding(.bottom, 20)
+            
+            TextField("Search via your postcode", text: $searchText)
+                .padding()
+                .background(Color(.systemCyan))
+                .cornerRadius(10)
+                .padding(.horizontal, 40) //check sizes
+            
+            Spacer()
+            
         }
-        .padding()
+    
+        .frame(maxHeight: .infinity) //Ensures this centered vertically
+            
+            
+        }
+    }
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
-#Preview {
-    ContentView()
-}
+
